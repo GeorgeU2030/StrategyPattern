66c1f008881e5d3510d15263ba0658e4e453bc0f
George Trujillo <georgeMike2030@outlook.com>
2022-09-15 22:08:44 -0500

    StrategyPattern

---
+++ src/ui/Division.java
@@ -0,0 +1,12 @@
+package ui;
+
+public class Division implements Strategy{
+
+	@Override
+	public int doOperation(int num1, int num2) {
+		return num1 /num2;
+	}
+	
+	
+
+}
---
+++ src/ui/Strategy.java
@@ -0,0 +1,6 @@
+package ui;
+
+public interface Strategy {
+	
+	public int doOperation(int num1, int num2);
+}
